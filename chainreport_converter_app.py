5d0b13b43e903955481de91e86dcb099b648f208
nacrul-eth <nacrul@klenk.bayern>
2023-10-24 14:05:48 +0200

    [app] Initial functional prototype application

--- chainreport_converter_app.py
+++ chainreport_converter_app.py
@@ -2,18 +2,12 @@
 
 import os
 
+# kivy dependencies
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+# program dependencies
 from chainreport_converter import ChainreportConverter
-from widgets.dropdown_button import DropdownButton
-
-class DropdownButton():
-    pass
 
 class MainScreen(Screen):
     """Main screen for the application"""
@@ -21,29 +15,49 @@ class MainScreen(Screen):
         super().__init__(**kwargs)
         self.parser = ""
         self.input_file = ""
-        self.output_file = "Test.csv"
+        self.local_file_path = ""
+        self.output_file = ""
+        self.converter = ""
 
     def open(self, path, filename):
-        with open(os.path.join(path, filename[0])) as f:
-            print(f.read())
+        """Store the full filename (including path) in local variable"""
+        if filename:
+            self.input_file = filename[0]
+            self.local_file_path = path
 
     def selected(self, filename):
+        """Store the selected filename """
         self.input_file = filename[0]
-        print("selected: %s" % filename[0])
+        self.log_action("You selected the input file " + self.input_file)
 
     def set_parser(self, parserstring):
+        """Set the parser for the conversion"""
         self.parser = parserstring
-        print(parserstring)
+        self.log_action("You selected the parser: " + self.parser)
+
+    def set_outputfile(self, full_filename_path):
+        """Set the output file"""
+        self.output_file = full_filename_path
+        self.log_action("You chose to write to the following file: " + self.output_file)
 
     def start_conversion(self):
-        self.input_file = "/home/michael/Finance/Crypto/Hi-statement-2.csv"
-        self.output_file = "Test.csv"
-        self.converter = ChainreportConverter(self.parser, self.input_file, self.output_file)
-        self.converter.convert()
+        """Start the conversion process, if all parameters are set"""
+        if self.parser and self.input_file and self.output_file:
+            self.converter = ChainreportConverter(self.parser, self.input_file, self.output_file)
+            self.log_action("Converting " + os.path.join(self.local_file_path, self.input_file) + " to " +
+                            os.path.join(self.local_file_path, self.output_file) + " using the Parser for " +
+                            self.parser)
+            self.converter.convert()
+            self.log_action("Conversion done!")
+        else:
+            self.log_action("Please select all input parameters first")
+
+    def log_action(self, loggingtext):
+        """Append text for logging on the GUI"""
+        self.ids.logging.text += loggingtext + "\n"
 
 class ChainreportConverterTool(ScreenManager):
     """Extensible screen manager implementation"""
-    pass
 
 class ChainreportConverterApp(App):
     """Main application"""
--- widgets/dropdown_button.py
+++ widgets/dropdown_button.py
@@ -3,8 +3,14 @@
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 
+# pylint: disable=no-name-in-module
+from kivy.properties import StringProperty
+
 class DropdownButton(Button):
     """Dropdown Button to select an element of a list (currently hardcoded)"""
+
+    selected_parser =  StringProperty('')
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.drop_list = None
@@ -14,9 +20,20 @@ class DropdownButton(Button):
 
         for parser in parsers:
             btn = Button(text=parser, size_hint_y=None, height=50)
+            # pylint: disable=E1101
             btn.bind(on_release=lambda btn: self.drop_list.select(btn.text))
 
             self.drop_list.add_widget(btn)
 
+        # pylint: disable=E1101
         self.bind(on_release=self.drop_list.open)
-        self.drop_list.bind(on_select=lambda instance, x: setattr(self, 'text', x))
+
+        self.drop_list.bind(on_select=lambda instance, parserstring: setattr(self, 'text', parserstring))
+        self.drop_list.bind(on_select=self.change_parser)
+
+    def change_parser(self, instance, parserstring):
+        """Change the parser proporty and the corresponding button text"""
+        del instance
+        # Change string property for external notification
+        # pylint: disable=E1101
+        self.selected_parser = parserstring
